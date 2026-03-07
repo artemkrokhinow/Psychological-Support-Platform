1d8d403413b6c3dab5e189b7d49cc331adfe929a
Agarenko <artkroh@gmail.com>
2026-03-07 23:21:56 +0100

    deploy test1

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ mongoose
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/materials", require("./routes/materials"));
 app.use("/api/scenarios", require("./routes/scenarios"));
-app.use("/api/users", require("./routes/users"));
+app.use("/api/users", require("./routes/user"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
